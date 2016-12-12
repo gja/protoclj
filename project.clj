63a8e19d4df6f138030fba5c73320f84b6f27ae9
Tejas Dinkar <tejas@gja.in>
2016-12-12 20:20:58 +0530

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protoclj "0.1.0"
+(defproject protoclj "0.2.0"
   :description "A protobuffer compiler for clojure wrappers"
   :url "https://github.com/nilenso/protoclj"
   :license {:name "The MIT License"}
